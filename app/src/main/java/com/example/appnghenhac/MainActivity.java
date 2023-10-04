5228e96aa62a17d1aa05a6d3c14a7393d5c5e1ae
Unknowless123 <uchihabitori@gmail.com>
2023-10-04 13:51:47 +0700

    add nav-header

--- app/src/main/java/com/example/appnghenhac/MainActivity.java
+++ app/src/main/java/com/example/appnghenhac/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.appnghenhac;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Khởi tạo các thành phần
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
